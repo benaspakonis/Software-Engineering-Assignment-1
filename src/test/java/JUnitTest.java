a553963032fcb21ae935e56a9a1820eae0213a81
Benas Pakonis <37660999+benaspakonis@users.noreply.github.com>
2020-11-24 19:37:29 +0000

    Created JUnit Tests

---
+++ src/test/java/JUnitTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import university.Student;
+
+public class JUnitTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+	
+	@Test
+	public void generateUsernameTest() {
+		Student student = new Student("John Bloggs", new DateTime(2000,1,1,0,0));
+		String str1 = "JohnBloggs1/1/2000";
+		assertEquals(student.getUsername(),str1);
+	}
+	
+	@Test
+	public void generateAgeTest() {
+		Student student = new Student("John Bloggs", new DateTime(2000,1,1,0,0));
+		int i = 20;
+		assertEquals(student.getAge(),i);
+	} 
+
+}
